322e404bd11e3037420a599f6332a55417d390b3
Vika Lab PC <viti.tr@gmail.com>
2024-05-09 13:38:13 +0300

    Update my_imgesc

--- my_imagesc.m
+++ my_imagesc.m
@@ -1,17 +1,28 @@
-function fig = my_imagesc(im)
+function [fig , hImage ]= my_imagesc(im,mask,ax)
 % Create figure with stretched image between 0.99 and 0.01 percentiles and equal axis
 
 im = double(im);
-fig = figure('Units','Normalized','Position',[0.05,0.05,0.6,0.8]); 
-imagesc(im); 
-[N,edges] = histcounts(im(:),1000,'Normalization','cdf');
-upperLim = ceil(edges(find(N > 0.99,1))); 
-lowerLim = floor(edges(find(N > 0.01,1)));
-if isequal(upperLim,lowerLim)
-    upperLim = lowerLim + 0.1;
+if nargin < 3
+    fig = figure('Units','Normalized','Position',[0.05,0.05,0.6,0.8]); 
+    hImage = imagesc(im); 
+    ax = gca;
+else
+    hImage = imagesc(ax,im);
+    fig = [];
 end
-SetAxisEqual();
-set(gca,'CLim',[ lowerLim upperLim]); 
+
+if nargin < 2
+    mask = true(size(im));
+end
+
+Lims = prctile(im(mask),[1, 99]);
+if isequal(Lims(1),Lims(2))
+    meanLims = mean(Lims);
+    Lims = meanLims + 0.5*[-1 1];
+end
+
+SetAxisEqual(ax);
+set(ax,'CLim',Lims); 
 colorbar;
 colormap gray;
 
@@ -19,6 +30,6 @@ function SetAxisEqual(ax)
 if nargin < 1
     ax = gca();
 end
-axis equal; 
+axis(ax,'equal'); 
 dataObj = findall(ax,'Type','Image');
 set(ax,'XLim', dataObj.XData + 0.5*[-1 1], 'YLim', dataObj.YData + 0.5*[-1 1])
